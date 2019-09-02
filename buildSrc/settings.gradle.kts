4add5bf10a59bcc0fce9c46788a151f5c15d2cca
Gabriel Harris-Rouquette <gabizou@me.com>
2019-09-02 07:21:17 -0700

    Migrate SpongeVanilla to a composite build and start reworking the build script to allow submodules.
    Signed-off-by: Gabriel Harris-Rouquette <gabizou@me.com>

---
+++ buildSrc/settings.gradle.kts
@@ -0,0 +1,7 @@
+//include("CommonBuildSrc", "SpongeAPIBuildSrc")
+//
+//val common = project(":CommonBuildSrc")
+//common.projectDir = file("../SpongeCommon/buildSrc")
+//val api = project(":SpongeAPIBuildSrc")
+//api.projectDir = file("../SpongeCommon/SpongeAPI/buildSrc")
+//
